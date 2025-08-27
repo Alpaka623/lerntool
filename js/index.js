aee6c024e196cc0f0fe6c5795e79c069731b6987
Niklas.Goltz <niklas.goltz@adesso.de>
2025-08-27 13:27:31 +0200

    cookies instead of localstorage

--- js/index.js
+++ js/index.js
@@ -129,15 +129,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function checkFirstVisit() {
-    if (!sessionStorage.getItem('notFirstTime')) {
+    if (!getCookie('lerntool_warning_shown')) {
         document.getElementById('firstVisitDialog').classList.remove('hidden');
         document.body.style.overflow = 'hidden';
     }
 }
 
 function closeFirstVisitDialog() {
-    sessionStorage.setItem('notFirstTime', 'true');
+    setCookie('lerntool_warning_shown', 'true', 365);
 
     document.getElementById('firstVisitDialog').classList.add('hidden');
 
